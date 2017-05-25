28f4a51afc4e481fb240563aa79ddb4b8c098e69
OsvaldoIG <osva_20@comunidad.unam.mx>
2017-05-24 23:10:06 -0500

    ranking sin nombres

---
+++ ranking.php
@@ -0,0 +1,36 @@
+<?php
+	 $con=mysqli_connect("localhost","root","","jsocial");
+		$query="SELECT * FROM ranglobal;";
+		$res=mysqli_query($con,$query);
+		$fila=mysqli_fetch_assoc($res); 
+		$g=array("0"=>"0","1"=>"0","2"=>"0","3"=>"0","4"=>"0","5"=>"0","6"=>"0","7"=>"0","8"=>"0","9"=>"0");
+		$usuario=array("0"=>"0","1"=>"0","2"=>"0","3"=>"0","4"=>"0","5"=>"0","6"=>"0","7"=>"0","8"=>"0","9"=>"0");
+		while($fila)
+		{
+			for($x=0;$x<=9;$x++)
+			{
+				if($fila['puntaje_altind']>=$g[$x])
+				{
+					for($w=9;$w>=$x+1;$w--)
+					{
+						$g[$w]=$g[$w-1];
+						$usuario[$w]=$usuario[$w-1];
+					}
+						$usuario[$x]=$fila['id_usuario'];
+						$g[$x]=$fila['puntaje_altind'];
+						$x=9;
+				}
+			}
+			$fila=mysqli_fetch_assoc($res);	
+		}
+		for($x=0;$x<=9;$x++)
+		{
+		print_r($x+1);
+		echo "°LUGAR----";
+		print_r($usuario[$x]);
+		echo "   Puntaje:";
+		print_r($g[$x]);
+		echo "<br/><br/>";	
+		}
+		
+?>
