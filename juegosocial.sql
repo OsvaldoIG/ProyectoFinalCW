a123c3be08189893b46262b792646b20c58c7be3
OsvaldoIG <osva_20@comunidad.unam.mx>
2017-05-24 23:52:28 -0500

    Update juegosocial.sql

--- juegosocial.sql
+++ juegosocial.sql
@@ -16,12 +16,298 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Current Database: `jsocial`
+-- Table structure for table `admin`
 --
 
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `jsocial` /*!40100 DEFAULT CHARACTER SET latin1 */;
+DROP TABLE IF EXISTS `admin`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `admin` (
+  `id_admin` int(10) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(15) DEFAULT NULL,
+  `correo` varchar(40) DEFAULT NULL,
+  `nombre_admin` varchar(15) DEFAULT NULL,
+  `fecha_nac` date DEFAULT NULL,
+  `contrasenia` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id_admin`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-USE `jsocial`;
+--
+-- Dumping data for table `admin`
+--
+
+LOCK TABLES `admin` WRITE;
+/*!40000 ALTER TABLE `admin` DISABLE KEYS */;
+/*!40000 ALTER TABLE `admin` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `clave`
+--
+
+DROP TABLE IF EXISTS `clave`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `clave` (
+  `clave` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `clave`
+--
+
+LOCK TABLES `clave` WRITE;
+/*!40000 ALTER TABLE `clave` DISABLE KEYS */;
+INSERT INTO `clave` VALUES ('adminavaljuesol5');
+/*!40000 ALTER TABLE `clave` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `comentarios`
+--
+
+DROP TABLE IF EXISTS `comentarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `comentarios` (
+  `id_comentario` int(11) NOT NULL AUTO_INCREMENT,
+  `id_publicacion` int(11) DEFAULT NULL,
+  `id_usuario` int(11) DEFAULT NULL,
+  `texto_comentario` varchar(800) DEFAULT NULL,
+  `tiempo_comentario` datetime DEFAULT NULL,
+  PRIMARY KEY (`id_comentario`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comentarios`
+--
+
+LOCK TABLES `comentarios` WRITE;
+/*!40000 ALTER TABLE `comentarios` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comentarios` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `imagenes`
+--
+
+DROP TABLE IF EXISTS `imagenes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `imagenes` (
+  `id_imagen` int(30) NOT NULL AUTO_INCREMENT,
+  `id_usuario` int(10) NOT NULL,
+  `imagen` longblob,
+  PRIMARY KEY (`id_imagen`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `imagenes`
+--
+
+LOCK TABLES `imagenes` WRITE;
+/*!40000 ALTER TABLE `imagenes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `imagenes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `juego`
+--
+
+DROP TABLE IF EXISTS `juego`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `juego` (
+  `id_usuarioret` int(10) DEFAULT NULL,
+  `id_usuarioopo` int(10) DEFAULT NULL,
+  `tipo_juego` char(1) DEFAULT NULL,
+  `puntaje_ret` int(4) DEFAULT NULL,
+  `puntaje_opo` int(4) DEFAULT NULL,
+  `num_partida` int(5) DEFAULT NULL,
+  `bar_ret5` char(14) DEFAULT NULL,
+  `bar_ret4` char(11) DEFAULT NULL,
+  `bar_ret3` char(8) DEFAULT NULL,
+  `bar_ret32` char(8) DEFAULT NULL,
+  `bar_ret2` char(5) DEFAULT NULL,
+  `bar_opo5` char(14) DEFAULT NULL,
+  `bar_opo4` char(11) DEFAULT NULL,
+  `bar_opo3` char(8) DEFAULT NULL,
+  `bar_opo32` char(8) DEFAULT NULL,
+  `bar_opo2` char(5) DEFAULT NULL,
+  `acierto_ret` char(50) DEFAULT NULL,
+  `fallas_ret` varchar(248) DEFAULT NULL,
+  `acierto_opo` char(50) DEFAULT NULL,
+  `fallas_opo` varchar(248) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `juego`
+--
+
+LOCK TABLES `juego` WRITE;
+/*!40000 ALTER TABLE `juego` DISABLE KEYS */;
+/*!40000 ALTER TABLE `juego` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `publicaciones`
+--
+
+DROP TABLE IF EXISTS `publicaciones`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `publicaciones` (
+  `id_usuario` int(10) DEFAULT NULL,
+  `id_publicacion` int(15) NOT NULL AUTO_INCREMENT,
+  `text_publicacion` varchar(500) DEFAULT NULL,
+  `fecha_publicacion` datetime DEFAULT NULL,
+  `tipo_publicacion` char(1) DEFAULT NULL,
+  PRIMARY KEY (`id_publicacion`),
+  KEY `id_usuario` (`id_usuario`),
+  CONSTRAINT `publicaciones_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `publicaciones`
+--
+
+LOCK TABLES `publicaciones` WRITE;
+/*!40000 ALTER TABLE `publicaciones` DISABLE KEYS */;
+/*!40000 ALTER TABLE `publicaciones` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ranglobal`
+--
+
+DROP TABLE IF EXISTS `ranglobal`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ranglobal` (
+  `id_usuario` int(10) DEFAULT NULL,
+  `puntaje_altind` int(4) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ranglobal`
+--
+
+LOCK TABLES `ranglobal` WRITE;
+/*!40000 ALTER TABLE `ranglobal` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ranglobal` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ranking`
+--
+
+DROP TABLE IF EXISTS `ranking`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ranking` (
+  `id_usuario` int(10) DEFAULT NULL,
+  `puntaje_altind` int(4) DEFAULT NULL,
+  `num_part` int(5) DEFAULT NULL,
+  KEY `id_usuario` (`id_usuario`),
+  CONSTRAINT `ranking_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ranking`
+--
+
+LOCK TABLES `ranking` WRITE;
+/*!40000 ALTER TABLE `ranking` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ranking` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `reportes`
+--
+
+DROP TABLE IF EXISTS `reportes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `reportes` (
+  `id_usuario` int(10) DEFAULT NULL,
+  `id_usurep` int(10) DEFAULT NULL,
+  `id_publicacion` int(15) DEFAULT NULL,
+  `texto_reporte` varchar(500) DEFAULT NULL,
+  KEY `id_usuario` (`id_usuario`),
+  CONSTRAINT `reportes_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `reportes`
+--
+
+LOCK TABLES `reportes` WRITE;
+/*!40000 ALTER TABLE `reportes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `reportes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usuarios` (
+  `id_usuario` int(10) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(15) DEFAULT NULL,
+  `correo` varchar(40) DEFAULT NULL,
+  `nombre_usu` varchar(15) DEFAULT NULL,
+  `fecha_nac` date DEFAULT NULL,
+  `contrasenia` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id_usuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-05-24 20:18:00
+-- MySQL dump 10.16  Distrib 10.1.21-MariaDB, for Win32 (AMD64)
+--
+-- Host: localhost    Database: localhost
+-- ------------------------------------------------------
+-- Server version	10.1.21-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `admin`
@@ -102,6 +388,29 @@ CREATE TABLE `publicaciones` (
 LOCK TABLES `publicaciones` WRITE;
 /*!40000 ALTER TABLE `publicaciones` DISABLE KEYS */;
 /*!40000 ALTER TABLE `publicaciones` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ranglobal`
+--
+
+DROP TABLE IF EXISTS `ranglobal`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ranglobal` (
+  `id_usuario` int(10) NOT NULL,
+  `puntaje_altind` int(5) DEFAULT NULL,
+  PRIMARY KEY (`id_usuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ranglobal`
+--
+
+LOCK TABLES `ranglobal` WRITE;
+/*!40000 ALTER TABLE `ranglobal` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ranglobal` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -191,4 +500,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-05-22 15:57:46
+-- Dump completed on 2017-05-24 23:50:21
