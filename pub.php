a49edd0f193937a8fbcfe118067d1ebd823b371e
ArlethLO <arleth.lo@comunidad.unam.mx>
2017-05-26 21:51:05 -0500

    Add files via upload

--- pub.php
+++ pub.php
@@ -1,23 +1,20 @@
 <?php
 $pubs=$_POST['publi'];
-echo $pubs;
 $con=mysqli_connect('localhost','root','','jsocial');
   if($con)
 	  $id_pu= mysqli_query($con,"SELECT * FROM publicaciones WHERE id_publicacion LIKE '%%'");
 	  $ip=mysqli_fetch_assoc($id_pu);
-	  print_r($ip);
 	  $n=1;
 	  while ($ip)
 	  {
 			$n++;
 			$ip=mysqli_fetch_assoc($id_pu);
 	  }
-	 
 	  $fecha= getdate();
 	  $anio= $fecha[year];
 	  $mes= $fecha[mon];
-	  $dia= $fecha[mday];
-	  $h= $fecha[hours]-7;
+	  $dia= $fecha[mday]-1;
+	  $h= $fecha[hours];
 	  $min= $fecha[minutes];
 	  $s= $fecha[seconds];
 	  $mi=strlen($min);
@@ -29,25 +26,21 @@ $con=mysqli_connect('localhost','root','','jsocial');
 	else 
 		$m=$min;
 	if($hor!=2)
-		$ho="0".$h;
+		$ho="0".$h+5;
 	else 
-		$ho=$h;
+		$ho=$h+5;
 	if($se!=2)
 		$sec="0".$s;
 	else 
 		$sec=$s;
 	if($me!=2)
-		$mess="0".$me;
+		$mess="0".$mes;
 	else 
-		$mess=$me;
+		$mess=$mes;
 	  $fech=$anio."-".$mess."-".$dia." ".$ho.":".$m.":".$sec;
 	  print_r($fecha);
 	 echo $fech;
 	 $insertar="INSERT INTO publicaciones(id_usuario, id_publicacion, text_publicacion, fecha_publicacion) VALUES ('0000000001','".$n."','".$pubs."','".$fech."');";
-	  $resultado= mysqli_query($con,$insertar);
-        if($resultado==FALSE)
-					echo "Error";
-        else
-          echo "ya";
-	echo "<meta http-equiv='refresh' content='0; ../templates/principalUsu.html'>";
+	  $resultado= mysqli_query($con,$insertar);  
+	echo "<meta http-equiv='refresh' content='0; ../templates/principalUsu.php'>";
 ?>
